ddb67a820b356c92958acb758677f87220499845
EarthenLynx <42932885+EarthenLynx@users.noreply.github.com>
2020-04-09 17:56:21 +0200

    Added sending image to temp folder

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Recipe = require("../../models/recipe");
 const path = require("path");
+const fs = require("fs");
 
 // Setup the router
 var router = express.Router();
@@ -14,59 +15,66 @@ mongoose.connect(mongoDB, { useNewUrlParser: true });
 // Get the list of recipes when calling the api with a get request.
 router.get("/", function (req, res, next) {
   Recipe.find({}, (err, recipes) => {
-    if(err) {
+    if (err) {
       res.send({
-        status: 500, 
-        statusText: "error", 
-        msg: "An error occured while fetching the recipes"
-      }); 
+        status: 500,
+        statusText: "error",
+        msg: "An error occured while fetching the recipes",
+      });
     }
-    if(!err) {
+    if (!err) {
       res.send({
-        status: 200, 
-        statusText: "success", 
+        status: 200,
+        statusText: "success",
         msg: `Found ${recipes.length} delicious recipes in database`,
-        data: recipes
-      })
+        data: recipes,
+      });
     }
+  });
+});
+
+// When receiving the post request, save the file in the temp folder
+router.post("/upload", (req, res, next) => {
+  let base64Data = req.body.img.replace(/^data:image\/png;base64,/, "");
+
+  fs.writeFile("./img/tmp/temp.png", base64Data, "base64", function (err) {
+    console.log(err);
   });
 });
 
 // Post a new recipe when receiving a post - request
 router.post("/", function (req, res, next) {
-  console.log("Now saving" + req.body.recipe_name);
-
-  let recipe = new Recipe({
-    recipe_img_url: path.join(
-      path.resolve(__dirname, "../../img/thumbnails"),
-      req.body.recipe_name
-    ),
-    recipe_name: req.body.recipe_name,
-    recipe_tags: req.body.recipe_tags,
-    recipe_desc: req.body.recipe_desc,
-    recipe_instructions: req.body.recipe_instructions,
-    recipe_dishes_qty: req.body.recipe_dishes_qty,
-    recipe_ingredients: req.body.recipe_ingredients,
-    recipe_time_minutes: req.body.recipe_time_minutes,
-    recipe_notes: req.body.recipe_notes,
-  });
-  recipe.save((err) => {
-    if (err) {
-      res.send({
-        status: 500, 
-        statusText: "error", 
-        msg: "Could not save recipe in database"
-      }); 
-      throw err;
-    };
-    if (!err) {
-      res.send({ 
-        status: 200,
-        statusText: "success",
-        msg: `Recipe '${req.body.recipe_name}' successfully saved!` 
-      });
-    }
-  });
+  // let recipe = new Recipe({
+  //   recipe_img_url: path.join(
+  //     path.resolve(__dirname, "../../img/thumbnails"),
+  //     req.body.recipe_name
+  //   ),
+  //   recipe_name: req.body.recipe_name,
+  //   recipe_tags: req.body.recipe_tags,
+  //   recipe_desc: req.body.recipe_desc,
+  //   recipe_instructions: req.body.recipe_instructions,
+  //   recipe_dishes_qty: req.body.recipe_dishes_qty,
+  //   recipe_ingredients: req.body.recipe_ingredients,
+  //   recipe_time_minutes: req.body.recipe_time_minutes,
+  //   recipe_notes: req.body.recipe_notes,
+  // });
+  // recipe.save((err) => {
+  //   if (err) {
+  //     res.send({
+  //       status: 500,
+  //       statusText: "error",
+  //       msg: "Could not save recipe in database"
+  //     });
+  //     throw err;
+  //   };
+  //   if (!err) {
+  //     res.send({
+  //       status: 200,
+  //       statusText: "success",
+  //       msg: `Recipe '${req.body.recipe_name}' successfully saved!`
+  //     });
+  //   }
+  // });
 });
 
 module.exports = router;
